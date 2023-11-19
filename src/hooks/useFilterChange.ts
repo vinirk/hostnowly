bba3e5dfe16604d2307db5561aa2d040e0709313
Vinícius Ramos Kawamoto <vinirk@gmail.com>
2023-11-19 20:42:06 -0300

    feat(filters): add useFilterChange custom hook
    - Implement `useFilterChange` hook for managing filter state updates
    - Include handleChangeFilter function for updating specific filter fields

---
+++ src/hooks/useFilterChange.ts
@@ -0,0 +1,24 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from 'app/store';
+import { FiltersState, setFilters } from 'features/filters/filtersSlice';
+
+/**
+ * Custom hook to handle filter changes.
+ * It provides a function to update filter state based on changed fields,
+ * utilizing Redux dispatch and selectors.
+ */
+export const useFilterChange = () => {
+  const dispatch = useDispatch();
+  const filter = useSelector((state: RootState) => state.filters);
+
+  /**
+   * Updates the filter state based on changed fields.
+   *
+   * @param {Partial<FiltersState>} updatedFields - An object containing the filter fields that have been changed.
+   */
+  const handleChangeFilter = (updatedFields: Partial<FiltersState>) => {
+    dispatch(setFilters({ ...filter, ...updatedFields }));
+  };
+
+  return handleChangeFilter;
+};
